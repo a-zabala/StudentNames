9248cbe03ce0c591738a4a1bf3d80c34ae2a6d13
Angelina Zabala <angekr@aol.com>
2018-10-01 16:38:57 -0500

    initial

--- StudentNames/Program.cs
+++ StudentNames/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace StudentNames
 {
@@ -6,7 +7,42 @@ namespace StudentNames
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int counter = 0;
+            string line;
+
+            // Reads the file  
+            StreamReader file =
+                new StreamReader(@"C:\Users\angekr\Documents\Visual Studio 2017\Projects\StudentNames\StudentNames\studentdata.txt");
+            
+            //Goes through the lines of data until no more lines
+            while ((line = file.ReadLine()) != null)
+            {
+                //Creates a student object
+                Student student = new Student();
+               //The line would output each line read from the file
+               // Console.WriteLine(line);
+               //Counts how many students are in the file
+                counter++;
+                    //This determines how many grades the student has 
+                    int grades = line.Split().Length - 1;
+                    string[] studentInfo = line.Split();
+                    
+                    student.Name = studentInfo[0];
+                    
+                   //Outputs only students that have 6 or more grades
+                   if  (grades >= 6)
+                    {
+                        
+                        Console.WriteLine(student.Name + " has " + grades + " grades");
+                    }
+                
+            }
+
+
+            file.Close();
+           Console.WriteLine("There were {0} students.", counter);
+            // Suspend the screen.  
+            Console.ReadLine();
         }
     }
 }
---
+++ StudentNames/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentNames
+{
+    //Creates a Student Class
+    public class Student
+    {
+        public string Name { get; set; }
+        //This will store the grades later
+        public List<int> Grades { get; set; }
+
+    }
+}
